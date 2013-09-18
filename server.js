24cf84265b65409693a4523590e03d31a6893dfa
Bert Spaan <bert.spaan@gmail.com>
2013-09-18 17:23:26 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+var sys = require("sys"),
+	os = require('os'),
+	pg = require("pg"),
+	util = require("util"),
+	mu = require("mu2"),
+	md = require("node-markdown").Markdown,
+	express = require("express"),
+	app = express(),
+	http = require("http"),
+	server = http.createServer(app);
+
+mu.root = __dirname + "/templates";
+
+var localhost = "localhost";
+var port = 8118;
+
+
+server.listen(port);
+
+app.use("/static", express.static(__dirname + "/static"));
+
+function render(res, filename) {
+	mu.clearCache();
+	var stream = mu.compileAndRender(filename, {});	
+	util.pump(stream, res);
+}
+
+app.get("/", function(req, res) {
+	render(res, "index.html");
+});
+
+app.get("/client", function(req, res) {
+	render(res, "client.html");
+});
+
+app.get("/server", function(req, res) {
+	render(res, "server.html");
+});
+
